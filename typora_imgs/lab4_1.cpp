f2d403c91552edde544084e1e04682a0858e72e3
zihengqin-cs <120879759+zihengqin-cs@users.noreply.github.com>
2023-03-16 18:43:25 +0800

    Upload by PicGo

---
+++ typora_imgs/lab4_1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+enum CPU_Rank { P1 = 1, P2, P3, P4, P5, P6, P7 };
+
+class CPU
+{
+private:
+	CPU_Rank rank;
+	int frequency;
+	float voltage;
+public:
+	CPU() {}
+	CPU(CPU_Rank r, int f, float v) {
+		rank = r;
+		frequency = f;
+		voltage = v;
+	}
+	~CPU() { cout << "析构掉一个CPU" << endl; }
+	CPU_Rank GetRank() const { return  rank; }
+	int GetFrequency()const { return frequency; }
+	float GetVoltage() const { return voltage; }
+
+	void SetRank(CPU_Rank r) { rank = r; }
+	void SetFrequency(int f) { frequency = f; }
+	void SetVoltage(float v) { voltage = v; }
+	void Run() { cout << "CPU开始运行！" << endl; }
+	void Stop() { cout << "CPU停止运行！" << endl; }
+
+};
+
+int main()
+{
+	CPU cpu;
+	cpu.Run();
+	cpu.Stop();
+}
